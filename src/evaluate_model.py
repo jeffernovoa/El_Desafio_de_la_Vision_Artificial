b634fcef98f1c177fe3066240f4f4bfe3737e535
jeffernovoa <jefferng35@gmail.com>
2025-10-19 22:02:47 +0200

    codigo actualizado

--- src/evaluate_model.py
+++ src/evaluate_model.py
@@ -1,16 +1,3 @@
-import matplotlib.pyplot as plt
-
 def evaluate_model(model, x_test, y_test):
-    test_loss, test_acc = model.evaluate(x_test, y_test)
-    print(f'Precisión en el conjunto de prueba: {test_acc:.2f}')
-
-def plot_history(history):
-    plt.plot(history.history['accuracy'], label='Precisión entrenamiento')
-    plt.plot(history.history['val_accuracy'], label='Precisión validación')
-    plt.xlabel('Épocas'); plt.ylabel('Precisión')
-    plt.legend(); plt.show()
-
-    plt.plot(history.history['loss'], label='Pérdida entrenamiento')
-    plt.plot(history.history['val_loss'], label='Pérdida validación')
-    plt.xlabel('Épocas'); plt.ylabel('Pérdida')
-    plt.legend(); plt.show()
+    test_loss, test_acc = model.evaluate(x_test, y_test, verbose=0)
+    return test_loss, test_acc
